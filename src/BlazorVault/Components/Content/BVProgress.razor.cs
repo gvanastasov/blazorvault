e72853ccc304696c787b93a0600e842dc8307322
Georgi <gvanastasov@gmail.com>
2019-10-19 19:31:27 +0200

    add progress bar

---
+++ src/BlazorVault/Components/Content/BVProgress.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorVault.Components.Content
+{
+	public class BVProgressBase : BVComponentBase
+	{
+		[Parameter]
+		public int Value { get; set; }
+
+		[Parameter]
+		public int Min { get; set; } = 0;
+
+		[Parameter]
+		public int Max { get; set; } = 100;
+
+		protected override bool Simple => false;
+
+		protected int LocalValue
+		{
+			get
+			{
+				return (int)(((Value - Min) * 1f / ( Max - Min )) * 100);
+			}
+		}
+	}
+}
