8f009705257302828564818e1952eeaaba30e5e2
Georgi <gvanastasov@gmail.com>
2019-10-19 11:45:17 +0200

    add carousel

---
+++ src/BlazorVault/Components/Content/BVCarousel.razor.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorVault.Components.Content
+{
+	public class BVCarouselBase : BVContentComponent
+	{
+
+		protected override bool Simple => false;
+
+		protected override string ClassBase => "carousel";
+
+		public LinkedListNode<BVCarouselSlide> Active;
+
+		public LinkedList<BVCarouselSlide> Slides { get; set; } = new LinkedList<BVCarouselSlide>();
+
+		private bool HasSlides
+		{
+			get
+			{
+				return this.Slides.Any();
+			}
+		}
+
+		private bool _showControls = true;
+		[Parameter]
+		public bool ShowControls
+		{
+			get
+			{
+				return this._showControls && this.Slides.Count > 1;
+			}
+			set
+			{
+				this._showControls = value;
+			}
+		}
+
+		[Parameter]
+		public bool Loop { get; set; }
+
+		protected bool HasPrev
+		{
+			get
+			{
+				return this.Loop || (this.ShowControls && this.Active.Previous != null);
+			}
+		}
+
+		protected bool HasNext
+		{
+			get
+			{
+				return this.Loop || (this.ShowControls && this.Active.Next != null);
+			}
+		}
+
+		private void Awake()
+		{
+			if (Active == null)
+			{
+				Active = Slides.First;
+			}
+
+			StateHasChanged();
+		}
+
+		public void Update()
+		{
+			this.StateHasChanged();
+		}
+
+		protected override void OnAfterRender(bool firstRender)
+		{
+			if (firstRender && HasSlides)
+			{
+				this.Awake();
+			}
+		}
+
+		public void Next()
+		{
+			if (HasNext)
+			{
+				Active = (Active.Next == null && Loop)
+					? Slides.First
+					: Active.Next;
+			}
+		}
+
+		public void Prev()
+		{
+			if (HasPrev)
+			{
+				Active = (Active.Previous == null && Loop)
+					? Slides.Last
+					: Active.Previous;
+			}
+		}
+	}
+}
+
---
+++ src/BlazorVault/Components/Content/BVCarouselSlide.cs
@@ -0,0 +1,43 @@
+﻿using BlazorVault.Components;
+using BlazorVault.Utils;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorVault
+{
+	public sealed class BVCarouselSlide : BVContentComponent
+	{
+		[Parameter]
+		public bool Active { get; set; }
+
+		protected override bool Simple => true;
+
+		protected override string ClassBase => "carousel-item";
+
+		[CascadingParameter]
+		private BVCarousel Parent { get; set; }
+
+		private bool IsActive
+		{
+			get
+			{
+				return Parent.Active.Value == this;
+			}
+		}
+
+		protected override void OnInitialized()
+		{
+			var self = Parent.Slides.AddLast(this);
+
+			if (Active)
+			{
+				Parent.Active = self;
+			}
+		}
+
+		protected override void GetClassString(CssBuilder builder)
+		{
+			base.GetClassString(builder);
+			builder.Add("active", IsActive);
+		}
+	}
+}
