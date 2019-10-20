0cf63a32617fb634069f17936e55ab71d4b75c76
Georgi <gvanastasov@gmail.com>
2019-10-20 09:54:26 +0200

    add jumbotron

---
+++ src/BlazorVault/Components/Content/BVJumbotron.cs
@@ -0,0 +1,22 @@
+﻿using BlazorVault.Components;
+using BlazorVault.Utils;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorVault
+{
+	public sealed class BVJumbotron : BVContentComponent
+	{
+		[Parameter]
+		public bool Fluid { get; set; }
+
+		protected override bool Simple => true;
+
+		protected override string ClassBase => "jumbotron";
+
+		protected override void GetClassString(CssBuilder builder)
+		{
+			base.GetClassString(builder);
+			builder.Add("jumbotron-fluid", this.Fluid);
+		}
+	}
+}
