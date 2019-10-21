7b6603e5437847b079615d92ce73d77de21d33c9
Georgi <gvanastasov@gmail.com>
2019-10-21 07:47:27 +0200

    add index animation

---
+++ app/BlazorVault.Web/Client/wwwroot/app.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+window['Blazor'].app = {
+    openGates: function (elementId) {
+        var element = document.getElementById(elementId);
+        setTimeout(function () {
+            element.classList.add("open");
+        }, 500);
+    }
+};
