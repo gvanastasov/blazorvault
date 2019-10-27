5a98ea982474ce2d5ad651880150de213e4d0193
Georgi <gvanastasov@gmail.com>
2019-10-27 14:56:55 +0100

    add link  component

---
+++ src/BlazorVault/Components/Common/BVLink.cs
@@ -0,0 +1,24 @@
+﻿using BlazorVault.Components;
+using BlazorVault.Constants;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorVault
+{
+	public sealed class BVLink : BVContentComponent
+	{
+		[Parameter]
+		public string To { get; set; } = "#";
+
+		protected override bool Simple => true;
+
+		protected override string DefaultTag => MarkupElements.Anchor;
+
+		protected override void AddAttributes(RenderTreeBuilder builder, ref int sequence)
+		{
+			base.AddAttributes(builder, ref sequence);
+
+			builder.AddAttribute(sequence++, "href", this.To);
+		}
+	}
+}
