711e3f4f46a2eda0a9a6420733d4f3d892320ec9
Georgi <gvanastasov@gmail.com>
2019-10-31 22:58:12 +0100

    add collection helper

--- src/BlazorVault/Enums/Style.cs
+++ src/BlazorVault/Enums/Style.cs
@@ -1,4 +1,8 @@
-﻿namespace BlazorVault
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorVault
 {
 	public enum Style
 	{
@@ -17,5 +21,15 @@
 		Light,
 
 		Dark,
+	}
+
+	public static class Styles
+	{
+		public static IEnumerable<Style> Collection()
+		{
+			return Enum
+				.GetValues(typeof(Style))
+				.Cast<Style>();
+		}
 	}
 }
