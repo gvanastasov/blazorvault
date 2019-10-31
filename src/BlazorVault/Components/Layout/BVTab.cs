74e552483b450c430b0b386105cd7f86751ef0d3
Georgi <gvanastasov@gmail.com>
2019-10-31 01:24:29 +0100

    add base tab system

---
+++ src/BlazorVault/Components/Layout/BVTab.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorVault.Components.Layout
+{
+	public class BVTabBase : BVContentComponent
+	{
+		protected override bool Simple => false;
+
+		public bool Active { get; set; }
+
+		[CascadingParameter]
+		private BVTabGroup Parent { get; set; }
+
+		[Parameter]
+		public RenderFragment Trigger { get; set; }
+
+		private bool IsActive
+		{
+			get
+			{
+				return Parent.Active == this;
+			}
+		}
+
+		protected override void OnInitialized()
+		{
+			Parent.Tabs.Add(this);
+
+			if (Active)
+			{
+				Parent.Select(this);
+			}
+		}
+	}
+}
---
+++ src/BlazorVault/Components/Layout/BVTabGroup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorVault.Components.Layout
+{
+	public class BVTabGroupBase : BVContentComponent
+	{
+		protected override bool Simple => false;
+
+		public IList<BVTabBase> Tabs { get; private set; } = new List<BVTabBase>();
+
+		public BVTabBase Active { get; private set; }
+
+		private bool HasTabs
+		{
+			get
+			{
+				return this.Tabs.Any();
+			}
+		}
+
+		private void Awake()
+		{
+			if (Active == null)
+			{
+				Active = Tabs.FirstOrDefault();
+			}
+
+			StateHasChanged();
+		}
+
+		public void Update()
+		{
+			this.StateHasChanged();
+		}
+
+		protected override void OnAfterRender(bool firstRender)
+		{
+			if (firstRender && HasTabs)
+			{
+				this.Awake();
+			}
+		}
+
+		public void Select(MouseEventArgs e, BVTabBase tab)
+		{
+			Select(tab);
+		}
+
+		public void Select(BVTabBase tab)
+		{
+			Active = tab;
+		}
+	}
+}
