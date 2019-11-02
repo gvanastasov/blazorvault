66f172d391ef4955278d5ecc283fab27a362a75a
Georgi <gvanastasov@gmail.com>
2019-11-02 17:51:03 +0100

    add dropdown and page

---
+++ src/BlazorVault/Components/Content/BVDropdown.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorVault.Components.Content
+{
+	public abstract class BVDropdownBase : BVContentComponent
+	{
+		protected override bool Simple => false;
+
+		protected override string ClassBase => "dropdown";
+
+		[Parameter]
+		public string Text { get; set; }
+	}
+}
---
+++ src/BlazorVault/Components/Content/BVDropdownItem.cs
@@ -0,0 +1,11 @@
+﻿using BlazorVault.Components;
+
+namespace BlazorVault
+{
+	public sealed class BVDropdownItem : BVContentComponent
+	{
+		protected override bool Simple => true;
+
+		protected override string ClassBase => "dropdown-item";
+	}
+}
