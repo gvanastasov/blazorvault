0eed7de7ca14cfd1d9a89879400b8ac619998ad7
Georgi <gvanastasov@gmail.com>
2019-11-03 12:33:30 +0100

    add tree structure component and page

---
+++ src/BlazorVault/Components/Content/BVTree.razor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorVault.Components.Content
+{
+	public class BVTreeBase : BVTreeNodeBase
+	{
+		protected override bool Simple => false;
+	}
+}
---
+++ src/BlazorVault/Components/Content/BVTreeNode.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorVault.Components.Content
+{
+	public class BVTreeNodeBase : BVContentComponent
+	{
+		protected override bool Simple => false;
+
+		[Parameter]
+		public string Caret { get; set; }
+
+		[Parameter]
+		public bool Expanded { get; set; }
+
+		[CascadingParameter]
+		public BVTreeNode Parent { get; set; }
+
+		[Parameter]
+		public RenderFragment Trigger { get; set; }
+
+		public List<BVTreeNodeBase> Children { get; set; } = new List<BVTreeNodeBase>();
+
+		public bool IsLeaf
+		{
+			get
+			{
+				return this.Children.Count == 0 && 
+					this.Trigger == null;
+			}
+		}
+
+		protected override void OnInitialized()
+		{
+			base.OnInitialized();
+
+			if (Parent != null)
+			{
+				Parent.Children.Add(this);
+			}
+		}
+
+		protected void Toggle()
+		{
+			Expanded = !Expanded;
+		}
+	}
+}
