d20d6de0ab2e58c52f062766e28cec98105945de
Georgi <gvanastasov@gmail.com>
2019-11-16 22:26:56 +0100

    add alert component code summary

--- src/BlazorVault/Components/Common/BVAlert.cs
+++ src/BlazorVault/Components/Common/BVAlert.cs
@@ -3,11 +3,22 @@ using Microsoft.AspNetCore.Components;
 
 namespace BlazorVault
 {
+	/// <summary>
+	/// Provide contextual feedback messages for typical user actions with the 
+	/// handful of available and flexible alert messages.
+	/// </summary>
 	public sealed class BVAlert : BVStyleComponent
 	{
+		/// <summary>
+		/// Describes the ability to dismiss/hide the alart message.
+		/// </summary>
 		[Parameter]
 		public bool Dismissible { get; set; }
 
+		/// <summary>
+		/// Returns a css style string in the format
+		/// of 'alert-{<see cref="Style"/>}'.
+		/// </summary>
 		protected override string VariantClass
 		{
 			get
@@ -18,12 +29,24 @@ namespace BlazorVault
 			}
 		}
 
+		/// <summary>
+		/// Defaults to <see cref="Style.Primary"/>.
+		/// </summary>
 		protected override Style? DefaultStyle => BlazorVault.Style.Primary;
 
+		/// <summary>
+		/// Results in bootstrap 'alert' CSS class.
+		/// </summary>
 		protected override string ClassBase => Modifiers.Elements.Alert;
 
+		/// <summary>
+		/// The components simple markup between open and end tags.
+		/// </summary>
 		protected override bool Simple => true;
 
+		/// <summary>
+		/// Adds 'alert' role to the element attributes.
+		/// </summary>
 		protected override string Role => Modifiers.Elements.Alert;
 	}
 }
