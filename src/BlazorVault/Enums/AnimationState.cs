5f1ddef799c33e125a352c552f4a6b6b43400547
Georgi <gvanastasov@gmail.com>
2019-11-19 07:49:48 +0100

    add animation state machine

---
+++ src/BlazorVault/Enums/AnimationState.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorVault
+{
+	public enum AnimationState
+	{
+		Idle = 0,
+
+		EnterStart = 1 << 0,
+
+		EnterEnd = 1 << 1,
+
+		EnterActive = EnterStart | EnterEnd,
+
+		LeaveStart = 1 << 2,
+
+		LeaveEnd = 1 << 3,
+
+		LeaveActive = LeaveStart | LeaveEnd,
+	}
+}
---
+++ src/BlazorVault/Utils/AnimationStateMachine.cs
@@ -0,0 +1,36 @@
+﻿using BlazorVault.Enums;
+
+namespace BlazorVault.Utils
+{
+	// UNDONE: WIP feature idea for handling transitions in components
+	public sealed class AnimationStateMachine
+	{
+		public delegate void SomethingHappened();
+
+		public event SomethingHappened OnEnter;
+
+		private AnimationState _currentState = AnimationState.Idle;
+
+		public AnimationState CurrentState
+		{
+			get
+			{
+				return _currentState;
+			}
+			set
+			{
+				StateHasChanged();
+				_currentState = value;
+			}
+		}
+
+		public void Enter()
+		{
+			this.CurrentState = AnimationState.EnterStart;
+		}
+
+		private void StateHasChanged()
+		{
+		}
+	}
+}
